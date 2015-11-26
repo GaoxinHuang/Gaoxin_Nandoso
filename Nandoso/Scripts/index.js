83dd3a4479fbb3478024310a892ab6ae31e27877
gx173139880 <gx173139880@qq.com>
2015-11-27 10:00:47 +1300

    https://github.com/gx173139880/Gaoxin_Nandoso.git

---
+++ Nandoso/Scripts/index.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    getSpecials();
+});
+
+function getSpecials() {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "specials.txt",
+        success: function (data) {
+            var rand = randomNum(12);
+            if (rand == 0) {
+                rand = 1;
+            }
+            $('#specialsHead1').html(data[rand].name);
+            $('#specialsImg1').attr("src", "img/specials" + rand + ".jpg");
+            var rand2 = rand + 1;
+            if (rand2 > 12) {
+                rand2 = 1;
+            }
+            $('#specialsHead2').html(data[rand2].name);
+            $('#specialsImg2').attr("src", "img/specials" + rand2 + ".jpg");
+        }
+    });
+}
+function randomNum(minNum) {
+    return Math.floor(Math.random() * minNum);
+}
